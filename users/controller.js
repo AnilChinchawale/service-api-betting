b90449034452da93083b50d0297a43364246e73d
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-08 11:18:24 +0530

    Add username & password checks

--- users/controller.js
+++ users/controller.js
@@ -15,23 +15,33 @@ const makePrediction = (req, res) => {
 };
 
 const signIn = (req, res) => {
+  validateAuthFields(req, res);
   const signInStatus = service.signIn(req.body.username, req.body.password);
-  if(signInStatus) {
-    return res.status(200).json({status: 200, message: 'Sign In successful'});
+  if (signInStatus) {
+    return res.status(200).json({ status: 200, message: 'Sign In successful' });
   }
   return errorResponse(res, "Sign In failed", new Error('signIn failed'), 401);
 };
 
 const signUp = (req, res) => {
+  validateAuthFields(req, res);
   const signUpStatus = service.signUp(req.body.username, req.body.password);
-  if(signUpStatus) {
-    return res.status(200).json({status: 200, message: 'Sign Up successful'});
+  if (signUpStatus) {
+    return res.status(200).json({ status: 200, message: 'Sign Up successful' });
   }
   return errorResponse(res, "Sign Up failed", new Error('signUp failed'), 401);
 };
 
 const errorResponse = (res, message, error, status = 500) =>
   res.status(status).json({ "status": status, "message": message, "error": error });
+
+const validateAuthFields = (req, res) => {
+  if (!req.body.hasOwnProperty('username') || req.body.username.length < 1 || typeof (req.body.username) != 'string') {
+    return errorResponse(res, "Invalid username", new Error('Invalid username'), 400);
+  } else if (!req.body.hasOwnProperty('password') || req.body.password.length < 1 || typeof (req.body.password) != 'string') {
+    return errorResponse(res, "Invalid password", new Error('Invalid password'), 400);
+  }
+};
 
 
 module.exports = {
--- users/users.spec.js
+++ users/users.spec.js
@@ -28,6 +28,75 @@ const userJane = {
 describe('User Tests', () => {
 
   describe('POST ' + apiRootURL + '/sign-up', () => {
+
+    // bad username
+    it('cannot sign up without username', (done) => {
+      chai.request(completeURL)
+        .post('/sign-up')
+        .send({ password: 'sdfsd' })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+
+    it('cannot sign up with blank username', (done) => {
+      chai.request(completeURL)
+        .post('/sign-up')
+        .send({ username: '', password: 'asdasd' })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+
+    it('cannot sign up with non-string username', (done) => {
+      chai.request(completeURL)
+        .post('/sign-up')
+        .send({ username: 1, password: 'asdasd' })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+
+    // bad password
+    it('cannot sign up without password', (done) => {
+      chai.request(completeURL)
+        .post('/sign-up')
+        .send({ username: 'sdfsd' })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+
+    it('cannot sign up with blank password', (done) => {
+      chai.request(completeURL)
+        .post('/sign-up')
+        .send({ usernam: 'dfsd', password: '' })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+
+    it('cannot sign up with non-string password', (done) => {
+      chai.request(completeURL)
+        .post('/sign-up')
+        .send({ username: 'sdfsdf', password: 1 })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.an('object');
+          done();
+        });
+    });
+
     it('user can sign up', (done) => {
       chai.request(completeURL)
         .post('/sign-up')
